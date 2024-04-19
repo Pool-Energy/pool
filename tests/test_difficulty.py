1f71a61acea68b9ccd883d7bb5bc0871334a5567
djerfy <djerfy@gmail.com>
2024-04-19 19:51:35 +0200

    feat: add expert difficulty test
    Signed-off-by: djerfy <djerfy@gmail.com>

--- tests/test_difficulty.py
+++ tests/test_difficulty.py
@@ -8,7 +8,6 @@ from pool.difficulty_adjustment import get_new_difficulty
 
 class TestDifficulty(unittest.TestCase):
     def test_no_things_in_db(self):
-
         time_target = 24 * 3600
         current_time = uint64(time.time())
         assert get_new_difficulty([], 300, time_target, 10, 'MEDIUM', current_time, 1) == 10
@@ -22,9 +21,7 @@ class TestDifficulty(unittest.TestCase):
             partials.append((uint64(current_time - (i) * 200), 20))
 
         assert get_new_difficulty(partials, num_partials, time_target, 50, 'MEDIUM', current_time, 1) == 50
-
         partials[0] = (current_time, 50)
-
         assert get_new_difficulty(partials, num_partials, time_target, 50, 'MEDIUM', current_time, 1) == 50
 
     def test_really_slow(self):
@@ -35,10 +32,7 @@ class TestDifficulty(unittest.TestCase):
         for i in range(num_partials):
             partials.append((uint64(current_time - (i + 100) * 200), 20))
 
-        # Decreases by 5x
         assert get_new_difficulty(partials, num_partials, time_target, 20, 'MEDIUM', current_time, 1) == 4
-
-        # Respects min difficulty
         assert get_new_difficulty(partials, num_partials, time_target, 20, 'MEDIUM', current_time, 10) == 10
 
     def test_kind_of_slow(self):
@@ -49,7 +43,6 @@ class TestDifficulty(unittest.TestCase):
         for i in range(num_partials):
             partials.append((uint64(current_time - (i + 20) * 200), 20))
 
-        # Decreases by 1.5x
         assert get_new_difficulty(partials, num_partials, time_target, 20, 'MEDIUM', current_time, 1) == (20 // 1.5)
 
     def test_not_enough_partials_yet(self):
@@ -58,12 +51,9 @@ class TestDifficulty(unittest.TestCase):
         partials = []
         current_time = uint64(time.time())
         for i in range(num_partials):
-            partials.append((uint64(current_time - (i) * 200), 20))
+            partials.append((uint64(current_time - i * 200), 20))
 
-        # Difficulty changed less than 2 hours ago, so do not change again
         partials[20] = (partials[20][0], 15)
-
-        # Doesn't change diff
         assert get_new_difficulty(partials, num_partials, time_target, 20, 'MEDIUM', current_time, 1) == 20
 
     def test_increases_diff(self):
@@ -105,6 +95,16 @@ class TestDifficulty(unittest.TestCase):
             partials.append((uint64(current_time - (i) * 200), 20))
 
         assert get_new_difficulty(partials, num_partials, time_target, 20, 'CUSTOM:5', current_time, 1) == 1733
+
+    def test_expert(self):
+        num_partials = 300
+        time_target = 24 * 3600
+        partials = []
+        current_time = uint64(time.time())
+        for i in range(num_partials):
+            partials.append((uint64(current_time - (i) * 200), 20))
+
+        assert get_new_difficulty(partials, num_partials, time_target, 20, 'EXPERT:5456', current_time, 1) == 5456
 
 if __name__ == "__main__":
     unittest.main()
