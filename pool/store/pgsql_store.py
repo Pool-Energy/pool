9b5b8a462ed92ab005405c6fd608076f479b2d96
djerfy <djerfy@gmail.com>
2024-06-15 22:49:40 +0200

    fix: use serealization and convert bytes to hexa (oops fix typo)
    Signed-off-by: djerfy <djerfy@gmail.com>

--- pool/store/pgsql_store.py
+++ pool/store/pgsql_store.py
@@ -412,8 +412,8 @@ class PgsqlPoolStore(object):
         error: Optional[str] = None,
     ) -> None:
         # Define plot filename (regex) from partial payload
-        plot_public_key = partial_payload.proof_of_space.plot_public_key.serealize()
-        pool_contract_puzzle_hash = partial_payload.proof_of_space.pool_contract_puzzle_hash.serealize()
+        plot_public_key = partial_payload.proof_of_space.plot_public_key.serialize()
+        pool_contract_puzzle_hash = partial_payload.proof_of_space.pool_contract_puzzle_hash.serialize()
         plot_id = std_hash(hexstr_to_bytes(pool_contract_puzzle_hash.hex()) + hexstr_to_bytes(plot_public_key.hex()))
         plot_filename = f"plot-k{partial_payload.proof_of_space.size}-*-{plot_id}.plot"
 
