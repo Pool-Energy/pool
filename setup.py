648cc4813e3950e1c00dfcd055f28414fcbd300d
djerfy <djerfy@gmail.com>
2025-01-29 21:05:26 +0100

    release v1.8.4
    Signed-off-by: djerfy <djerfy@gmail.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="pool",
-    version="1.8.3",
+    version="1.8.4",
     author="🌱 DJΞRFY 🚀",
     author_email="djerfy@gmail.com",
     description=("Pool.Energy of Chia blockchain."),
