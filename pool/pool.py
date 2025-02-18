d7a3ba00a52bceaf3be528d20631a47ea97af772
djerfy <djerfy@gmail.com>
2025-02-18 20:20:52 +0100

    chore: revert to default compare of chia version
    Signed-off-by: djerfy <djerfy@gmail.com>

--- pool/pool.py
+++ pool/pool.py
@@ -1838,7 +1838,7 @@ class Pool:
 
         # Verify chia version and refuse partials if the version is not supported by pool
         if not self.testnet and req_metadata and chia_version_current_using and chia_version_refuse_before:
-            if Version(chia_version_current_using).release < Version(chia_version_refuse_before).release:
+            if Version(chia_version_current_using) < Version(chia_version_refuse_before):
                 await self.partials.add_partial(
                     partial.payload,
                     req_metadata,
