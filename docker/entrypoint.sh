4e413f772096f52719820cf18d8a4cd2dc44e7bd
djerfy <djerfy@gmail.com>
2026-09-21 17:35:30 +0200

    feat: run chia init if the chia rootdir is missing/empty
    Signed-off-by: djerfy <djerfy@gmail.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,7 @@ trap "killall python" TERM
 simpleproxy -d -L 127.0.0.1:25 -R ${MAIL_HOSTNAME:=mail}:25
 
 if [ ! -e "${CHIA_ROOT}/config/config.yaml" ]; then
-    ./venv/bin/chia init --root-path ${CHIA_ROOT}
+    ./venv/bin/chia init
 fi
 
 exec ./venv/bin/python \
